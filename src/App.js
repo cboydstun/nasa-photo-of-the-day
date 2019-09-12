77c5563a004a8d8d26247cbc5c1ea247d17ef93b
Chris Boydstun <chrisboydstun@gmail.com>
2019-09-12 18:58:26 -0500

    styled

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from "react";
 import axios from 'axios'
+import styled from 'styled-components';
+
 import "./App.css";
 import PhotoCard from './components/PhotoCard'
 
@@ -10,7 +12,7 @@ function App() {
 
   useEffect(() => {
     // Make a request for a user with a given ID
-    axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${date}`)
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=TzkL1Z80xN72XdKRbv6UiTtS3kDgm0eyCPgziLlf&date=${date}`)
       .then( res =>{
         // handle success
         console.log(res);
@@ -26,6 +28,8 @@ function App() {
 
   return (
     <div className="App">
+
+      
 
 
       {cardState
--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -1,13 +1,64 @@
 import React from 'react'
+import styled from 'styled-components';
+
+const WrapperDiv = styled.div`
+  font-family: sans-serif;
+  text-align: center;
+  margin: 0 auto;
+  background-color:white;
+  padding: 20px;
+`;
+
+const BodyDiv = styled.div`
+  width: 500px;
+  background-color: grey;
+  padding: 50px;
+  align-items: center;
+  justify-content: center;
+  margin: auto;
+  border-radius: 20%;
+  border-width: 10px;
+  border-style: solid;
+`;
+
+const Headline = styled.h1`
+  text-align:center;
+  color: black;
+  font-size: 40px;
+`;
+
+const SpacePic = styled.img`
+  height: 300px;
+  width: 300px;
+`;
+
+const SpaceBlurb = styled.span`
+  width: 500px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: auto;
+`;
+
+const SpaceDate = styled.p`
+  font-size: 40px;
+  display: flexbox;
+  align-items: center;
+  justify-content: center;
+  margin:0;
+`;
+
 
 function PhotoCard(props){
   return (
-    <div>
-      <h2>{props.title}</h2>
-      <img src={props.url} alt="nasa"/>
-      <p>{props.explanation}</p>
-      <p>{props.date}</p>
-    </div>
+    <WrapperDiv>
+      <BodyDiv>
+        <Headline>{props.title}</Headline>
+        <SpaceDate>{props.date}</SpaceDate>
+        <SpacePic src={props.url} alt="nasa"/>
+        <SpaceBlurb>{props.explanation}</SpaceBlurb>
+      </BodyDiv>
+    </WrapperDiv>
   )
 
 }
